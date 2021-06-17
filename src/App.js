639d326a90fd42e253a492a466f464fad3149c45
Sajith Pradeep <sajith.pradeep1993@gmail.com>
2021-06-18 02:00:29 +0530

    forwardRefs and useRef hooks examples

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 //import Parent from "./Parent";
-import SlideShow from "./SlideShow";
+//import SlideShow from "./SlideShow";
+import UseRefHookExplained from "./components/UseRefHook/UseRefHookExplained";
+
+const centerContent = {
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  alignItems: "center",
+  background: "#333",
+  color: "#f4f4f4",
+  height: "100vh",
+};
 
 function App() {
   return (
     <div className="App">
-      <SlideShow>
+      {/* <SlideShow>
         <img src="./assets/1.jpg" alt="" width="500" />
         <img src="./assets/2.jpg" alt="" width="500" />
         <img src="./assets/3.jpg" alt="" width="500" />
-      </SlideShow>
+      </SlideShow> */}
+      <UseRefHookExplained style={centerContent} />
     </div>
   );
 }
